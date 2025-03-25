5986665d58ffc6b4ea88f6dce8b619bc063eb2bd
Armando <armando@Armando.local>
2025-03-25 13:18:39 -0600

    Metodos del Store

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -18,10 +18,61 @@ const state = {
 
 const initStore = () => {
     console.log(state);
-    console.log('InitStore -#-')
+    console.log('InitStore 🥑')
+}
+
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = ( description ) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId Identificador del ToDo
+ */
+const toggleTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId Identificador del ToDo 
+ */
+const deleteTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+}
+
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {Filters} newFilter Filtro de los ToDo
+ */
+const setFilter = ( newFilter = Filters.All ) => {
+    throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
 }
 
 
 export default {
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
